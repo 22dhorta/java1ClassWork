bc1eb47e2bea82feec1bed686dffc4f23f7b2990
22dhorta <diegoh@scollege.cl>
2019-07-26 16:43:21 -0700

    test
    it

--- MathOperations/src/Calculator/Numbers.java
+++ MathOperations/src/Calculator/Numbers.java
@@ -25,14 +25,39 @@ public class Numbers {
       System.out.println("    Ex. 6 % 4 = the remainder of 6/4 or 2.");
       System.out.println("Factorial (!): Multiplies a number by each whole number below it down to one");
       System.out.println("    Ex. 5! = 5 * 4 * 3 * 2 * 1");
+      System.out.println("Square Root (S): Finds the number that, if squared, would equal the input");
        }
   else { }
   System.out.println("Enter first number:");
-  num1 = keyboard.nextDouble();
-  System.out.println("Enter second number:");
-  num2 = keyboard.nextDouble();
+  num1 = keyboard.nextDouble(); 
   System.out.println("What operation do you want done?");
   Operator = keyboard.next().charAt(0);
+  if (Operator == '!' || Operator == 'S') {
+      double num11 = num1;
+      switch (Operator) {
+          case '!':
+              for (int i = 0; i < num11 - 1; i++) {
+              num1 = num1 * (num11 - (i + 1));
+//              System.out.println(i);
+//              System.out.println(num1);
+          }
+              answer = num1;  
+          case 'S': answer = Math.sqrt(num1);
+      }
+      System.out.println("Answer:");
+      if (Operator == '!') {
+          System.out.println(num1);
+      }
+      else { 
+          System.out.println("√" + num1 + " = " + answer);
+      }
+      
+  }
+  
+  else {
+       System.out.println("Enter second number:");
+  num2 = keyboard.nextDouble();
+ 
   
    
   switch (Operator) {
@@ -52,6 +77,8 @@ public class Numbers {
   }
   
   System.out.println(num1 + " " + Operator + " " + num2 + " = " + answer);
+  }
+ 
   
 
       
